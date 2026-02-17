31d02d87c714ac9ec06966313042528d8402f3a9
kuzmichov45-ua <kuzmichov45@gmail.com>
2026-02-17 16:30:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,20 +64,14 @@ async def send_new_poll(chat_id):
         new_msg = await bot.send_message(chat_id, render_text(votes, current_limit), reply_markup=get_keyboard())
         last_poll_msg_id = new_msg.message_id
 
-# УЛУЧШЕННАЯ ПРОВЕРКА АДМИНА
 async def is_admin(message: types.Message):
     member = await message.chat.get_member(message.from_user.id)
     if member.is_chat_admin():
         return True
     else:
-        # 1. Сразу удаляем команду не-админа
         try: await message.delete()
         except: pass
-        
-        # 2. Пишем предупреждение
         warning = await message.answer(f"❌ {message.from_user.first_name}, управлять ботом могут только администраторы группы! ⚽️")
-        
-        # 3. Удаляем предупреждение через 5 секунд
         await asyncio.sleep(5)
         try: await warning.delete()
         except: pass
@@ -107,8 +101,7 @@ async def up_player(message: types.Message):
     
     if len(args) == 2 and all(a.isdigit() for a in args):
         res_idx, main_idx = int(args[0]) - 1, int(args[1]) - 1
-
-reserve = all_yes[current_limit:]
+        reserve = all_yes[current_limit:]
         main = all_yes[:current_limit]
         
         if 0 <= res_idx < len(reserve) and 0 <= main_idx < len(main):
@@ -116,7 +109,7 @@ reserve = all_yes[current_limit:]
             votes[r_id]['time'], votes[m_id]['time'] = votes[m_id]['time'], votes[r_id]['time']
             save_votes(votes)
             await send_new_poll(message.chat.id)
-
+            
 @dp.message_handler(commands=['excel'])
 async def get_excel(message: types.Message):
     if not await is_admin(message): return

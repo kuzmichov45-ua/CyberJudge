c6bc55f4fadf0f542ca499408199b3f907951be8
kuzmichov45-ua <kuzmichov45@gmail.com>
2026-02-18 11:29:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 import logging
 from aiogram import Bot, Dispatcher
-from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,12 +13,5 @@ API_TOKEN = os.getenv("TOKEN")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# Flask для Render
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "I am alive"
-
 def run():
-    app.run(host='0.0.0.0', port=10000)
+    pass
